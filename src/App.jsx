e2c3f30c9c21fc7b208535b8781d63044168cce1
Abhishek Kumar <abhiabhishekrkt1996@gmail.com>
2026-02-15 07:10:28 +0530

    maggie in progress

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Hero from './components/Hero'
+import Slider from './components/Slider'
+
+function App() {
+
+
+  return (
+    <>
+     <Hero /> 
+     <Slider />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+     <nav className='navbar'>
+     <ul className='nav-links left'>
+        <li><a href="#about">About</a></li>
+        <li><a href="#recipes"></a>Recipes</li>
+        <li><a href="#recipes">Stories</a></li>
+        
+
+        </ul>    
+        <div className='logo'>
+            <img src="./images/logo.png" alt="logo" />
+        </div>
+          
+    <ul className='nav-links right'>
+        <li><a href="#shop">Shop</a></li>
+        <li><a href="#contact"></a>Contact</li>
+        <li><a href="#contact">Flavour</a></li>
+        
+        </ul>  
+
+     </nav>
+
+     
+    
+  )
+}
+
+export default Navbar
